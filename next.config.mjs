770756e50d9959562088b70476d381b7b9fe2b46
TechPertz <reetnandy@gmail.com>
2025-01-27 21:02:57 -0500

    config update and data update

--- next.config.mjs
+++ next.config.mjs
@@ -46,30 +46,8 @@ const nextConfig = {
         destination: '/#resume',
         permanent: true,
       },
-    ]
+    ];
   },
-}
+};
 
-mergeConfig(nextConfig, userConfig)
-
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      }
-    } else {
-      nextConfig[key] = userConfig[key]
-    }
-  }
-}
-
-export default nextConfig
+export default nextConfig;
